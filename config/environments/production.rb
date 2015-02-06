98b0d02e120322bd9957b721ee7c7e310d7c9452
Tunde Agboola <tun.agboola@gmail.com>
2015-02-06 14:11:53 -0500

    updated gem file and attempting to fix assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,8 @@ ApWebsite::Application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
+  config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
